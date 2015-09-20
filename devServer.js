4cdf6eac31520e80c724e3fd3835ddd8d3284d49
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-09-20 17:36:09 +0200

    Added colors to devServer output

--- devServer.js
+++ devServer.js
@@ -8,7 +8,10 @@ let devServer = new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     inline: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    stats: {
+        colors: true
+    }
 });
 
 devServer.listen(8080, 'localhost', (err) => {
