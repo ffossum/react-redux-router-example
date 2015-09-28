4b81f18ba66bdc15785d3b338e583ed05fc5fdf7
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-09-28 22:19:40 +0200

    Replaced hard coded menu items with loop

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,22 +3,24 @@ import { Link } from 'react-router';
 
 class Menu extends React.Component {
   render() {
+    const links = [
+      {href: '/bmi', text: 'BMI'},
+      {href: '/bmi-reactive', text: 'BMI (Reactive)'},
+      {href: '/about', text: 'About'},
+      {href: '/login', text: 'Log in'}
+    ];
+
     return (
       <nav className="pure-menu pure-menu-horizontal">
         <Link className="pure-menu-heading pure-menu-link" to="/">Front page</Link>
         <ul className="pure-menu-list">
-          <li className="pure-menu-item">
-            <Link className="pure-menu-link" key="bmi" to="/bmi">BMI</Link>
-          </li>
-          <li className="pure-menu-item">
-            <Link className="pure-menu-link" key="bmi" to="/bmi-reactive">BMI (Reactive)</Link>
-          </li>
-          <li className="pure-menu-item">
-            <Link className="pure-menu-link" key="about" to="/about">About</Link>
-          </li>
-          <li className="pure-menu-item">
-            <Link className="pure-menu-link" key="login" to="/login">Log in</Link>
-          </li>
+          {links.map(link => {
+            return (
+              <li className="pure-menu-item">
+                <Link className="pure-menu-link" key={link.href} to={link.href}>{link.text}</Link>
+              </li>
+            )
+          })}
         </ul>
       </nav>
     )
